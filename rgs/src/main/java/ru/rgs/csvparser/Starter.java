d536a413e664c239a85a15e045aa5a75adae65cf
Aleksey Mikhaylov <al.a.mikhaylov@gmail.com>
2019-07-03 07:21:36 +0300

    Adding

--- rgs/src/main/java/ru/rgs/csvparser/Starter.java
+++ rgs/src/main/java/ru/rgs/csvparser/Starter.java
@@ -2,9 +2,10 @@ package ru.rgs.csvparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import ru.rgs.csvparser.configuration.MainConfiguration;
+
 
 @EnableFeignClients
 @SpringBootApplication
--- rgs/src/test/java/ru/rgs/csvparse/MainTest.java
+++ rgs/src/test/java/ru/rgs/csvparse/MainTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.rgs.csvparser.Starter;
